1796914fb6460e096b9a53cc738b4c12c3cb610c
luiz <luiz.scheidegger@gmail.com>
2013-03-23 09:28:12 -0700

    First commit

---
+++ src/com/lfscheidegger/jfacet/activity/JFacetDemoActivity.java
@@ -0,0 +1,17 @@
+package com.lfscheidegger.jfacet.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.lfscheidegger.jfacet.R;
+
+public class JFacetDemoActivity extends Activity {
+
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
